a8158797dfb18352c199b549a616c71f59d08419
Jean Folle Vanz <jeanfolle86@gmail.com>
2025-06-15 16:14:34 -0300

    feat: adiciona controle de conexões com connected_clients

--- app/websocket_handler.py
+++ app/websocket_handler.py
@@ -1,10 +1,14 @@
 import tornado.websocket
 
+connected_clients = set()
+
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
+        connected_clients.add(self)
         print("Aberto")
 
     def on_close(self):
+        connected_clients.remove(self)
         print("Fechado")
     
     def check_origin(self, origin):
