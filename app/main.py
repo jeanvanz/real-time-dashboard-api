c78ac87c90f5ce63f49f3efbbe6592806d8d257b
bernardosfz <bernardofattini@gmail.com>
2025-06-26 08:10:08 -0300

    separado redis_listner do main

--- app/main.py
+++ app/main.py
@@ -1,23 +1,12 @@
 import asyncio
 import tornado.websocket
 import tornado.web
-import redis
 from websocket_handler import WebSocketHandler
+from redis_listener import redisListener
 
-def redis_listener():
-    r = redis.Redis(
-    host='redis-17263.c10.us-east-1-4.ec2.redns.redis-cloud.com',
-    port=17263,
-    password='MtAMMqqMoWwrmwe6bvmQ9b38Sj4yA4Yj',
-    ssl=False
-)
-    pubsub = r.pubsub()
-    pubsub.subscribe('real_time_dashboard')
-    
-    for message in pubsub.listen():
-        if message and message['type'] == 'message':
-            data = message['data']
-            print(data)
+def read_send_data():
+    data = redisListener()
+    print(data)
 
 def make_app():
     return tornado.web.Application([
@@ -27,8 +16,8 @@ def make_app():
 async def main():
     app = make_app()
     app.listen(8888, address="0.0.0.0")
-    redis_listener()
     print("Server rodando")
+    read_send_data()
 
     await asyncio.Event().wait()
 
--- app/redis_listener.py
+++ app/redis_listener.py
@@ -1,9 +1,18 @@
-#import redis
-#from config.redis_config import REDIS_CONFIG
-#
-#r = redis.Redis(**REDIS_CONFIG)
-#
-#success = r.set('foo', 'bar')
-#
-#result = r.get('foo')
-#print(result)
+import redis
+from config.redis_config import REDIS_CONFIG
+
+# def redisListener():
+#     data = 'preciso pescar'
+#     return data
+
+def redisListener():
+    r = redis.Redis(**REDIS_CONFIG)
+    pubsub = r.pubsub()
+    pubsub.subscribe('real_time_dashboard')
+    
+    for message in pubsub.listen():
+        if message and message['type'] == 'message':
+            data = message['data']
+            
+            return data
+
