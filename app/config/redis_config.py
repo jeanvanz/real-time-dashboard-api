87a166a761f87eeee5b8fe146b976278526f191a
Otávio Lorenzatto <lorenzattoo@gmail.com>
2025-06-25 22:50:12 -0300

    comentado arquivos redis_listener.py e redis_config.py

--- app/config/redis_config.py
+++ app/config/redis_config.py
@@ -1,15 +1,15 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+#import os
+#from os.path import join, dirname
+#from dotenv import load_dotenv
 
-dotenv_path = join (dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+#dotenv_path = join (dirname(__file__), '.env')
+#load_dotenv(dotenv_path)
 
-REDIS_CONFIG = {
-    "host" : os.environ.get("REDIS_HOST"),
-    "port" : os.environ.get("REDIS_PORT"),
-    "db" : os.environ.get("REDIS_DB", 0),
-    "username" : os.environ.get("REDIS_USERNAME"),
-    "password" : os.environ.get("REDIS_PASSWORD"),
-    "decode_responses" : True
-}
+#REDIS_CONFIG = {
+#    "host" : os.environ.get("REDIS_HOST"),
+#    "port" : os.environ.get("REDIS_PORT"),
+#    "db" : os.environ.get("REDIS_DB", 0),
+#    "username" : os.environ.get("REDIS_USERNAME"),
+#    "password" : os.environ.get("REDIS_PASSWORD"),
+#    "decode_responses" : True
+#}
