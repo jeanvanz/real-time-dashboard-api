a1f9ee043f5c12fd619bd6b73c8970e14a1567ee
bernardosfz <bernardofattini@gmail.com>
2025-06-27 09:02:00 -0300

    busca e envia dados referente ao uso de disco do computador

--- app/redis_pub.py
+++ app/redis_pub.py
@@ -9,7 +9,8 @@ r = redis.Redis(**REDIS_CONFIG)
 while True:
     data = {
         'cpu': psutil.cpu_percent(),
-        'ram': psutil.virtual_memory().percent
+        'ram': psutil.virtual_memory().percent,
+        'disk': psutil.disk_usage('/').percent
     }
     r.publish('real_time_dashboard', json.dumps(data))
     print(data)
